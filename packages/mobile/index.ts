056be69c69b886198db3b10a56ec4376944206ef
x0s3 <x0s3.prog@gmail.com>
2019-07-04 14:23:19 +0200

    -Mobile commit

---
+++ packages/mobile/index.ts
@@ -0,0 +1,5 @@
+import { App } from 'components/src';
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+const path = require('path');
+
+module.exports = {
+  watchFolders: [
+    // To allow finding files outside mobile
+    path.resolve(__dirname, '../..')
+  ],
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: true,
+        inlineRequires: true
+      }
+    })
+  }
+};
